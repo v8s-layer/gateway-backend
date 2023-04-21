4b4948a33e5bab630b0e6031b0d4abb91c06b12a
lukepark327 <lukepark327@gmail.com>
2023-04-21 21:53:33 +0900

    chore: example of chat (sample)

---
+++ examples/chat.js
@@ -0,0 +1,8 @@
+function chat(params) {
+    const { prompt } = params;
+    const res = "Project Gateway is a groundbreaking solution designed to bridge the gap between Web 2.0 and Web 3.0, built on the Archway platform.";
+    return res;
+}
+
+// Expose the function to be executed by the server
+mainFunction = chat;
